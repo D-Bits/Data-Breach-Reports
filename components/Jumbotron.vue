85d9c5b806233759bc770e4a5fcc1aa94b31f12a
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2021-01-23 17:06:52 -0800

    Renamed Header component to Jumbotron.

---
+++ components/Jumbotron.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="jumbotron">
+      <h2 class="main-title">Data Breach Reports</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.main-title {
+    text-align: center;
+    color: aliceblue;
+}
+
+.jumbotron {
+    background-color: #346da3;
+    border-bottom: 5px solid #2f404e;
+    height: 7.5%;
+    padding: 2.5%;
+}
+
+</style>
